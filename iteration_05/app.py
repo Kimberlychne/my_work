3c622ae468ea1c24ab88c935cdac93fa5af9ab83
Kimberlychne <ychen26@nmhschool.org>
2025-11-05 20:12:44 -0500

    Update app.py

--- iteration_05/app.py
+++ iteration_05/app.py
@@ -10,20 +10,21 @@ def home():
 def contact():
     email = "ychen@nmhschool.org"
     phone = "857-654-8955"
-    github = "https://github.com/Kimberlychne/my_work"
+    github = "https://github.com/Kimberlycheng/my_work"
     return render_template("contact.html", email=email, phone=phone, github=github)
 
 @app.route("/about")
 def about():
     author = "Kimberly"
     favorite_language = "Python"
-    interests = "Web Development", "Game Development"
+    interests = ["Web Development", "Game Development"]
     is_student = True
-    return render_template("about.html", 
-                           author=author,
-                           favorite_language=favorite_language,
-                           interests=interests,
-                           is_student=is_student
-                            )
-if __name__ =="__main__":
-    app.run(debug=True)
+    return render_template("about.html",
+        author=author,
+        favorite_language=favorite_language,
+        interests=interests,
+        is_student=is_student
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5001)
